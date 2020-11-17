9b6698053abdf5f7d4f7d279732ca2d7569375b2
hymntaha <tahauygun@gmail.com>
2020-11-16 23:13:14 -0800

    adding preorder, inorder and postorder

---
+++ BinaryTree/binary_tree.py
@@ -0,0 +1,45 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def preorder_print(self, start, traversal):
+        """Root->Left-> right"""
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+
+        return traversal
+
+    def inorder_print(self, start, traversal):
+        """Left->Root->Right"""
+        if start:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal += (str(start.value)+ "-")
+            traversal = self.inorder_print(start.right, traversal)
+
+        return traversal
+
+    def postorder_print(self, start, traversal):
+        """Left->Right->Root"""
+        if start:
+            traversal = self.postorder_print(start.left, traversal)
+            traversal = self.postorder_print(start.right, traversal)
+            traversal += (str(start.value) + "-")
+
+        return traversal
+
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
