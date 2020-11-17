29b51fc8695b04d7bbd1dc8357b0faad7351a27f
hymntaha <tahauygun@gmail.com>
2020-11-17 13:34:46 -0800

    BST completed

--- BinaryTree/binary_search_tree.py
+++ BinaryTree/binary_search_tree.py
@@ -4,45 +4,90 @@ class Node(object):
         self.left = None
         self.right = None
 
+
 class BST(object):
     def __init__(self, root):
         self.root = Node(root)
 
-    def insert(self, new_val):
-        self.insert_helper(self.root, new_val)
-
-    def insert_helper(self, current, new_val):
-        if current.data < new_val:
-            if current.right:
-                self.insert_helper(current.right, new_val)
-            else:
-                current.right = Node(new_val)
-
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data)
         else:
-            if current.left:
-                self.insert_helper(current.left, new_val)
+            self._insert(data, self.root)
+
+    def _insert(self, data, cur_node):
+        if data < cur_node.data:
+            if cur_node.left is None:
+                cur_node.left = Node(data)
             else:
-                current.left = Node(new_val)
+                self._insert(data, cur_node.left)
 
-    def search(self, find_val):
-        return self.search_helper(self.root, find_val)
+        elif data > cur_node.data:
+            if cur_node.right is None:
+                cur_node.right = Node(data)
+            else:
+                self._insert(data, cur_node.right)
+        else:
+            print("Value already in tree!")
 
-    def search_helper(self, current, find_val):
-        if current:
-            if current.data == find_val:
+    def inorder_print_tree(self):
+        if self.root:
+            self._inorder_print_tree(self.root)
+
+    def _inorder_print_tree(self, cur_node):
+        if cur_node:
+            self._inorder_print_tree(cur_node.left)
+            print(str(cur_node.data))
+            self._inorder_print_tree(cur_node.right)
+
+    def find(self, data):
+        if self.root:
+            is_found = self._find(data, self.root)
+            if is_found:
                 return True
-            elif current.data < find_val:
-                return self.search_helper(current.right, find_val)
-            else:
-                return self.search_helper(current.left, find_val)
+            return False
+        else:
+            return None
 
+    def _find(self, data, cur_node):
+        if data > cur_node.data and cur_node.right:
+            return self._find(data, cur_node.right)
+        elif data < cur_node.data and cur_node.left:
+            return self._find(data, cur_node.left)
+        if data == cur_node.data:
+            return True
 
-bst = BST(10)
-bst.insert(3)
-bst.insert(1)
-bst.insert(25)
-bst.insert(9)
-bst.insert(13)
+    def is_bst_satisfied(self):
+        def helper(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
 
-print(bst.search(9))
-print(bst.search(14))
+            val = node.data
+            if val <= lower or val >= upper:
+                return False
+
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(self.root)
+
+bst = BST(4)
+bst.insert(2)
+bst.insert(8)
+bst.insert(5)
+bst.insert(10)
+
+tree = BST(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+tree.root.right.right.right = Node(8)
+
+print(bst.is_bst_satisfied())
+print(tree.is_bst_satisfied())
