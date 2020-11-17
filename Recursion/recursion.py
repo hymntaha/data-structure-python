c3409568272828923926a8d7f0358e576d353b11
hymntaha <tahauygun@gmail.com>
2020-11-17 13:41:06 -0800

    uppercase find iterative and recursive

---
+++ Recursion/recursion.py
@@ -0,0 +1,24 @@
+def find_uppercase_iterative(input_str):
+    for i in range(len(input_str)):
+        if input_str[i].isupper():
+            return input_str[i]
+    return "No uppercase character found"
+
+def find_uppercase_recursive(input_str, idx=0):
+    if input_str[idx].isupper():
+        return input_str[idx]
+    if idx == len(input_str) - 1:
+        return "No uppercase character found"
+    return find_uppercase_recursive(input_str, idx+1)
+
+input_str_1 = "lucidProgramming"
+input_str_2 = "LucidProgramming"
+input_str_3 = "lucidprogramming"
+
+print(find_uppercase_iterative(input_str_1))
+print(find_uppercase_iterative(input_str_2))
+print(find_uppercase_iterative(input_str_3))
+
+print(find_uppercase_recursive(input_str_1))
+print(find_uppercase_recursive(input_str_2))
+print(find_uppercase_recursive(input_str_3))
