79929b3953002b00876431da7319f4be6f44416b
hymntaha <tahauygun@gmail.com>
2020-11-17 14:18:11 -0800

    count string

--- Recursion/recursion.py
+++ Recursion/recursion.py
@@ -11,14 +11,22 @@ def find_uppercase_recursive(input_str, idx=0):
         return "No uppercase character found"
     return find_uppercase_recursive(input_str, idx+1)
 
-input_str_1 = "lucidProgramming"
-input_str_2 = "LucidProgramming"
-input_str_3 = "lucidprogramming"
+# input_str_1 = "lucidProgramming"
+# input_str_2 = "LucidProgramming"
+# input_str_3 = "lucidprogramming"
+#
+# print(find_uppercase_iterative(input_str_1))
+# print(find_uppercase_iterative(input_str_2))
+# print(find_uppercase_iterative(input_str_3))
+#
+# print(find_uppercase_recursive(input_str_1))
+# print(find_uppercase_recursive(input_str_2))
+# print(find_uppercase_recursive(input_str_3))
 
-print(find_uppercase_iterative(input_str_1))
-print(find_uppercase_iterative(input_str_2))
-print(find_uppercase_iterative(input_str_3))
+def print_len(str):
+    if str == '':
+        return 0
+    print(print_len(str[1:]))
+    return 1 + print_len(str[1:])
 
-print(find_uppercase_recursive(input_str_1))
-print(find_uppercase_recursive(input_str_2))
-print(find_uppercase_recursive(input_str_3))
+print(print_len('tacotuesday'))
