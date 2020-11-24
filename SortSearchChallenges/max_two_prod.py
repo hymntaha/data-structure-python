61b1fdd53b414fe70d552c35649a696977d2311e
hymntaha <tahauygun@gmail.com>
2020-11-24 14:09:44 -0800

    fix

--- SortSearchChallenges/max_two_prod.py
+++ SortSearchChallenges/max_two_prod.py
@@ -1,9 +1,7 @@
+# Decimal library to assign infinite numbers
 from decimal import Decimal
 
 def find_max_prod(lst):
-    # sorted_lst = sorted(lst)
-    # return  sorted_lst[-2], sorted_lst[-1]
-
     max1 = lst[0]
     max2 = Decimal('-Infinity')
 
@@ -13,25 +11,26 @@ def find_max_prod(lst):
     for number in lst:
 
         if number > max1:
-            max2 = max1 # second highest
-            max1 = number # first highest
-
+            max2 = max1  # Second highest
+            max1 = number  # First highest
         elif number > max2:
             max2 = number
 
         if number < min1:
-            min2 = min1 # Second lowest
-            min1 = number # First lowest
+            min2 = min1  # Second lowest
+            min1 = number  # First lowest
         elif number < min2:
             min2 = number
 
-        if max1 * max2 > min1 * min2:
-            return max2, max1
-        else:
-            return min2, min1
+    # Checking which pair has the highest product
+    if max1 * max2 > min1 * min2:
+        return max2, max1
+    else:
+        return min2, min1
 
 
 lst = [1, 3, 5, 2, 6]
-print(find_max_prod(lst))
+num1, num2 = find_max_prod(lst)
+print(num1, num2)
 
 # We have traversed the list in a single loop. So, the time complexity is O(n)
