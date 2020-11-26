ad79104d1cf3a054a4bc629b82ed0818095559cf
hymntaha <tahauygun@gmail.com>
2020-11-26 13:31:51 -0800

    calculate number of nodes

--- Graph_Algos/graph.py
+++ Graph_Algos/graph.py
@@ -33,14 +33,66 @@ class Graph:
                 temp = temp.next
             print(" \n")
 
-V = 5  # Total vertices
+# V = 5  # Total vertices
+# g = Graph(V)
+# g.add_edge(0, 1)
+# g.add_edge(0, 4)
+# g.add_edge(1, 2)
+# g.add_edge(1, 3)
+# g.add_edge(1, 4)
+# g.add_edge(2, 3)
+# g.add_edge(3, 4)
+#
+# g.print_graph()
+
+def number_of_nodes(my_graph, level):
+    source = 0
+
+    # Mark all the vertices as not visited
+    visited = [0] * (len(my_graph.graph))
+
+    # Create a queue
+    queue = []
+
+    # Result string
+    result = ""
+
+    # Mark the source node as
+    # visited and enqueue it
+
+    queue.append(source)
+    visited[source] = 1
+
+    while queue:
+
+        # Dequeue a vertex from queue
+        source = queue.pop(0)
+
+        # Get all adjacent vertices of the
+        # dequeued vertex source. If a adjacent
+        # has not been visited, then mark it
+        # visited and enqueue it
+
+        while my_graph.graph[source] is not None:
+            data = my_graph.graph[source].vertex
+            if visited[data] == 0:
+                queue.append(data)
+                visited[data] = visited[source] + 1
+            my_graph.graph[source]= my_graph.graph[source].next
+
+    # Counting number of nodes at given level
+    result = 0
+    for i in range(len(my_graph.graph)):
+        if visited[i] == level:
+            result += 1
+    return result
+
+
+V = 5
 g = Graph(V)
 g.add_edge(0, 1)
-g.add_edge(0, 4)
-g.add_edge(1, 2)
+g.add_edge(0, 2)
 g.add_edge(1, 3)
 g.add_edge(1, 4)
-g.add_edge(2, 3)
-g.add_edge(3, 4)
 
-g.print_graph()
+print(number_of_nodes(g, 2))
