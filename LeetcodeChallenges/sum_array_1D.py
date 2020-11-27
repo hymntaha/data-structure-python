fd6e8ecafca6ce343f5126e642fe83de2c8837e4
hymntaha <tahauygun@gmail.com>
2020-11-27 00:31:02 -0800

    faster solution with list

--- LeetcodeChallenges/sum_array_1D.py
+++ LeetcodeChallenges/sum_array_1D.py
@@ -1,11 +1,20 @@
 def runningSum(nums):
-    i = 1
-    while i<len(nums):
-        nums[i]+=nums[i-1]
-        i+=1
-    return nums
+    # i = 1
+    # while i<len(nums):
+    #     nums[i]+=nums[i-1]
+    #     i+=1
+    # return nums
 
+    # Faster solution with O(N) space complexity
+    n = len(nums)
+    result = list()
 
+    currentSum = 0
+    for i in range(n):
+        currentSum += nums[i]
+        result.append(currentSum)
+
+    return result
 
 nums = [3,1,2,10,1]
 print(runningSum(nums))
