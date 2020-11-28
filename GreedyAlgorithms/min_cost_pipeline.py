538087506466cbf0705e63411e8fdefe661cd274
hymntaha <tahauygun@gmail.com>
2020-11-28 13:11:20 -0800

    solution for pipe costs

---
+++ GreedyAlgorithms/min_cost_pipeline.py
@@ -0,0 +1,17 @@
+def min_cost(pipes):
+    cost = 0
+    n = len(pipes)
+    pipes.sort() # Sorting the list using the .sort() built-in function
+
+    for i in range(n-1):
+        prev_cost = cost # store previos cost for later use
+        cost = (pipes[i]+pipes[i+1]) # find current cost
+        pipes[i+1] = cost # insert in list
+
+        cost = cost +prev_cost # add with previous cost
+
+    return cost
+
+
+pipes = [4, 3, 2, 6]
+print(min_cost(pipes))
