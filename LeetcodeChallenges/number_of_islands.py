e96d62cc334bc495848a75e03ca3184ec3219512
hymntaha <tahauygun@gmail.com>
2020-11-30 15:37:44 -0800

    solution for top frequent words

--- LeetcodeChallenges/number_of_islands.py
+++ LeetcodeChallenges/number_of_islands.py
@@ -8,16 +8,18 @@ def numIslands(grid):
             if grid[i][j] == '1':
                 dfs(grid, i, j)
                 count += 1
+
     return count
 
 def dfs(grid, i, j):
-    if i<0 or  j<0 or i>= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
-        return
+    if (i>=len(grid) or i < 0 or j < 0 or j >= len(grid[0]) or grid[i][j] != '1'):
+        return 0
     grid[i][j] = '#'
     dfs(grid, i+1, j)
     dfs(grid, i-1, j)
     dfs(grid, i, j+1)
     dfs(grid, i, j-1)
+
 
 grid = [
     ["1","1","1","1","0"],
---
+++ LeetcodeChallenges/top_k_frequent.py
@@ -0,0 +1,15 @@
+def topKFrequent(words, k):
+    dict = {}
+    for x in words:
+        if x in dict:
+            dict[x] += 1
+        else:
+            dict[x] = 1
+    res = sorted(dict, key=lambda x: (-dict[x], x))
+    return res[:k]
+
+
+input = ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"]
+k = 4
+print(topKFrequent(input,k))
+
