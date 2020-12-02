334def2178f9c868aeac5ad060559481400c4193
hymntaha <tahauygun@gmail.com>
2020-12-02 13:48:01 -0800

    adding complexity analysis

--- SlidingWIndow/fruit_basket.py
+++ SlidingWIndow/fruit_basket.py
@@ -23,3 +23,8 @@ def fruits_into_baskets(fruits):
 
 fruits=['A', 'B', 'C', 'B', 'B', 'C']
 print(fruits_into_baskets(fruits))
+
+# time complexity is O(N) where N is the number of chars in the input array.
+# The outer for loop runs for all chars and the inner while loop processes each character only once, therefore, the time complexity of the algo is O(N+N)
+# which is asymptotically equivalent to O(N)
+# Space complexity - constant - O(1) as there can be a max of three types of fruits stored in frequency map.
