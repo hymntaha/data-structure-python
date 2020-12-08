32112a747e05a708ee68bc615cfa3cf5bb3b054c
taha uygun <tahauygun@gmail.com>
2020-12-08 14:44:33 -0800

    quick refactoring

--- dynamic-programming-js/fib-memoized.js
+++ dynamic-programming-js/fib-memoized.js
@@ -1,15 +1,15 @@
-const fib = (n) => {
+const fibMemoized = (n) => {
   if (n <= 2) {
     return 1;
   }
-  return fib(n - 1) + fib(n - 2);
+  return fibMemoized(n - 1) + fibMemoized(n - 2);
 };
 
 // Memoize version
 const fib2 = (n, memo={}) => {
   if (n in memo) return memo[n];
   if(n<=2) return 1;
-  memo[n] = fib2(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = fib2(n - 1, memo) + fibMemoized(n - 2, memo);
   return memo[n]
 }
 //
