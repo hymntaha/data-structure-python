41b60f4e987ca772952400c29183c4f5b5e30005
taha uygun <tahauygun@gmail.com>
2020-12-18 13:16:37 -0800

    solution triplet smaller than target

---
+++ TwoPointer/triplet_smaller_sum.py
@@ -0,0 +1,20 @@
+def triplet_with_smaller_sum(arr, target):
+    arr.sort()
+    count = 0
+
+    for i in range(len(arr) - 2):
+        start = i + 1
+        end = len(arr) - 1
+
+        while (start <  end):
+            sum = arr[i] + arr[start] + arr[end]
+
+            if(sum<target):
+                count = count + end - start;
+                start += 1
+            else:
+                end -= 1
+
+    return count
+
+print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))
