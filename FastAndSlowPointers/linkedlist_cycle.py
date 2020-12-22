587fd10df9e3d47d6ba9a18b42823af8bc2a9de2
taha uygun <tahauygun@gmail.com>
2020-12-22 15:53:41 -0800

    solution for linked list cycle

---
+++ FastAndSlowPointers/linkedlist_cycle.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+def has_cycle(head):
+    slow, fast = head, head
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+
+        if slow == fast:
+            return True # Found a cycle
+    return False
+
+
+
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+head.next.next.next.next.next = Node(6)
+print("LinkedList has cycle: " + str(has_cycle(head)))
+
+head.next.next.next.next.next.next = head.next.next
+print("LinkedList has cycle: " + str(has_cycle(head)))
+
+head.next.next.next.next.next.next = head.next.next.next
+print("LinkedList has cycle: " + str(has_cycle(head)))
