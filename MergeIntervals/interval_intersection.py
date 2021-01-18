6b2089bffb96025bde13678a00c24b70a613c5a2
taha uygun <tahauygun@gmail.com>
2021-01-18 14:45:54 -0800

    solution space and time complexity

--- MergeIntervals/interval_intersection.py
+++ MergeIntervals/interval_intersection.py
@@ -3,12 +3,17 @@ def merge(intervals_a, intervals_b):
     i, j, start, end = 0,0,0,1
 
     while i < len(intervals_a) and j < len(intervals_b):
+        # check if intervals overlap and intervals_a[i]' start time lies within the other intervals_b[j]
         a_overlaps_b = intervals_a[i][start] >= intervals_b[j][start] and intervals_a[i][start] <= intervals_b[j][end]
+
+        # check if intervals overlap and intervals_a[j]'s start time lies within the other intervals_b[i]
         b_overlaps_a = intervals_b[j][start] >= intervals_a[i][start] and intervals_b[j][start] <= intervals_a[i][end]
 
+        # store the the intersection part
         if(a_overlaps_b or b_overlaps_a):
             result.append([max(intervals_a[i][start], intervals_b[j][start]), min(intervals_a[i][end], intervals_b[j][end])])
 
+        # move next from the interval which is finishing first
         if intervals_a[i][end] < intervals_b[j][end]:
             i += 1
         else:
@@ -23,3 +28,10 @@ def main():
 
 
 main()
+
+"""
+As we are iterating through both the lists once, the time complexity of the above algorithm is O(N+M), where 'N' and 'M'
+are the total number of intervals in the input arrays respectively.
+
+Space Complexity: Ignoring the space needed for the result list, the algorithm runs in constant space O(1)
+"""
