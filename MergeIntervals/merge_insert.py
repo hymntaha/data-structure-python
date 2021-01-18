5f3d98f1d13d47caedf33966e583d986cbf3b48b
taha uygun <tahauygun@gmail.com>
2021-01-18 12:28:34 -0800

    some practice

--- MergeIntervals/merge_insert.py
+++ MergeIntervals/merge_insert.py
@@ -1,22 +1,18 @@
 def insert(intervals, new_interval):
     merged = []
-    i, start, end = 0, 0, 1
+    i, start, end = 0,0,1
 
-    # skip (and add to output) all intervals that come before the 'new_interval'
-    while i < len(intervals) and intervals[i][end] < new_interval[start]:
+    while i < len(intervals) and intervals[i][end] <= new_interval[start]:
         merged.append(intervals[i])
         i += 1
 
-    # merge all intervals that overlap with 'new_interval'
-    while i < len(intervals) and intervals[i][start] <= new_interval[end]:
+    while i < len(intervals) and intervals[i][start]<=new_interval[end]:
         new_interval[start] = min(intervals[i][start], new_interval[start])
         new_interval[end] = max(intervals[i][end], new_interval[end])
         i += 1
 
-    # insert the new_interval
     merged.append(new_interval)
 
-    # add all the remaining intervals to the output
     while i < len(intervals):
         merged.append(intervals[i])
         i += 1
@@ -40,3 +36,4 @@ where ‘N’ is the total number of intervals.
 Space complexity
 The space complexity of the above algorithm will be O(N) as we need to return a list containing all the merged intervals.
 """
+
--- MergeIntervals/merge_intervals.py
+++ MergeIntervals/merge_intervals.py
@@ -1,15 +1,19 @@
+from __future__ import print_function
+
+
 class Interval:
     def __init__(self, start, end):
         self.start = start
         self.end = end
-    def print_interval(self):
-        print("[" + str(self.start) + ', ' + str(self.end) + "]", end='')
 
-def merge_intervals(intervals):
-    if len(intervals) < 2:
+    def print_interval(self):
+        print("[" + str(self.start) + ", " + str(self.end) + "]", end='')
+
+
+def merge(intervals):
+    if(len(intervals) < 2):
         return intervals
 
-    # sort the intervals on the start time
     intervals.sort(key=lambda x:x.start)
 
     merged = []
@@ -18,20 +22,20 @@ def merge_intervals(intervals):
 
     for i in range(1,len(intervals)):
         interval = intervals[i]
-        if interval.start <= end: # overlapping intervals, adjust the end
+        if interval.start <= end:
             end = max(interval.end, end)
-        else: # non-overlapping interval, add the previous interval and reset
+        else:
             merged.append(Interval(start,end))
             start = interval.start
             end = interval.end
-    merged.append(Interval(start, end))
+    merged.append(Interval(start,end))
     return merged
 
-print("Merged intervals: ", end='')
-for i in merge_intervals([Interval(1, 4), Interval(2, 5), Interval(7, 9)]):
-    i.print_interval()
-print()
 
-# Time complexity: O(N * logN) where N is tthe total number of intervals. Iterating the intervals only once will take O(N),
-# in the beginning though, since we need to sort the intervals, algorithm will take O(N*logN)
-# Space complexity: O(N) for sorting
+def main():
+    print("Merged intervals: ", end='')
+    for i in merge([Interval(1, 4), Interval(2, 5), Interval(7, 9)]):
+        i.print_interval()
+    print()
+
+main()
