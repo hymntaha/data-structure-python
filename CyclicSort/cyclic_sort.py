07a9d33af4834308699f7cf1c76084e24971b388
taha uygun <tahauygun@gmail.com>
2021-01-19 19:20:50 -0800

    space and time complexity

--- CyclicSort/cyclic_sort.py
+++ CyclicSort/cyclic_sort.py
@@ -1,7 +1,7 @@
 def cyclic_sort(nums):
     i = 0
 
-    while i<len(nums):
+    while i < len(nums):
         j = nums[i] - 1
         if nums[i] != nums[j]:
             nums[i], nums[j] = nums[j], nums[i],
@@ -12,3 +12,13 @@ def cyclic_sort(nums):
 
 input = [2,6,4,3,1,5]
 print(cyclic_sort(input))
+
+"""
+The time complexity of the algo is O(N). Although we are not incrementing the index i when
+swapping the numbers, this will result in more than 'n' iterations of the loop, but in the worst-case
+scenario, the while loop will swap a total of n-1 numbers and once a number is at its correct index,
+we will move on to the next number by incrementing i. So overall, our algorithm will take O(n) + O(n-1) which is
+asymptotically equivalent to O(n).
+
+Space complexity: The algo runs in constant space O(1)
+"""
