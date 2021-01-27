45c51db569e84846563523314287d77763badd65
Taha Uygun <tahauygun@gmail.com>
2021-01-26 21:06:43 -0800

    reverse sub list solution

---
+++ ReverseLinkedList/revere-sublist.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+
+def reverse_sub_list(head, p, q):
+    # TODO: Write your code here
+    return head
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
+    result = reverse_sub_list(head, 2, 4)
+    print("Nodes of reversed LinkedList are: ", end='')
+    result.print_list()
+
+
+main()
