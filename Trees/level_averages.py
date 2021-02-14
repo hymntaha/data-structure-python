3a817fdd8a6d99decf6bd4d0c39068a7344ac1d0
Taha Uygun <tahauygun@gmail.com>
2021-02-14 10:12:17 -0800

    level size average

---
+++ Trees/level_averages.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+def find_level_averages(root):
+    result = []
+
+    if root is None:
+        return result
+
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+        levelSize = len(queue)
+        levelSum = 0.0
+
+        for _ in range(levelSize):
+            currentNode = queue.popleft()
+            levelSum += currentNode.val
+
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.left)
+
+        result.append(levelSum/levelSize)
+
+    return result
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level averages are: " + str(find_level_averages(root)))
+
+
+main()
