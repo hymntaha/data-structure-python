8bcb7cf6b436f6738ac0a5c7f075e22911321b97
Taha Uygun <tahauygun@gmail.com>
2021-02-16 23:14:09 -0800

    two heap start

--- DFS/all_path_for_sum.py
+++ DFS/all_path_for_sum.py
@@ -49,5 +49,8 @@ main()
 """
 TIME COMPLEXITY: O(N^2), where N is the total number of nodes in the tree. This
 is due to the fact that we traverse each node once (which will take O(N)), and
-for every leaf node, we might have to store its path (by making a copy of
+for every leaf node, we might have to store its path (by making a copy of current path)
+which will take O(N).
+
+SPACE COMPLEXITY: O(N)
 """
---
+++ TwoHeaps/median_number_stream.py
@@ -0,0 +1,22 @@
+class MedianOfAStream:
+    def insert_num(self, num):
+        # TODO: Write your code here
+        return -1
+
+    def find_median(self):
+        # TODO: Write your code here
+        return 0.0
+
+
+def main():
+    medianOfAStream = MedianOfAStream()
+    medianOfAStream.insert_num(3)
+    medianOfAStream.insert_num(1)
+    print("The median is: " + str(medianOfAStream.find_median()))
+    medianOfAStream.insert_num(5)
+    print("The median is: " + str(medianOfAStream.find_median()))
+    medianOfAStream.insert_num(4)
+    print("The median is: " + str(medianOfAStream.find_median()))
+
+
+main()
