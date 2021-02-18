36c9d1126b456bc24ff6c72a36fcfeff0ced6861
Taha Uygun <tahauygun@gmail.com>
2021-02-17 22:04:20 -0800

    heap median solution

--- TwoHeaps/median_number_stream.py
+++ TwoHeaps/median_number_stream.py
@@ -1,12 +1,30 @@
+from heapq import *
+
 class MedianOfAStream:
+    maxHeap = [] # containing first half of numbers
+    minHeap = [] # containing second half of numbers
+
     def insert_num(self, num):
-        # TODO: Write your code here
-        return -1
+        if not self.maxHeap or -self.maxHeap[0] >= num:
+            heappush(self.maxHeap, -num)
+        else:
+            heappush(self.minHeap, num)
+
+        # either both the heaps will have equal number of elements or max-heap will have one
+        # more element than the min-heap
+
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            heappush(self.minHeap, -heappop(self.maxHeap))
+        else:
+            heappush(self.maxHeap, -heappop(self.min))
 
     def find_median(self):
-        # TODO: Write your code here
-        return 0.0
+        if len(self.maxHeap) == len(self.minHeap):
+            # we have even number of elements, take the average of middle two elements
+            return -self.maxHeap[0] / 2.0 + self.minHeap[0] / 2.0
 
+        # Because max-heap will have one more element than the min-heap
+        return -self.maxHeap[0] / 1.0
 
 def main():
     medianOfAStream = MedianOfAStream()
