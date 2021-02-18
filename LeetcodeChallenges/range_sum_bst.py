f167b94e4ed763edc02e6a72a836f6d67d3abb5c
Taha Uygun <tahauygun@gmail.com>
2021-02-17 23:10:58 -0800

    start of sum bst

---
+++ LeetcodeChallenges/range_sum_bst.py
@@ -0,0 +1,8 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
