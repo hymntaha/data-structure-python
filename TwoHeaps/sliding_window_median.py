4a06c5910794fc1f2d3b261891cb1c203afc4e29
Taha Uygun <tahauygun@gmail.com>
2021-02-17 22:27:38 -0800

    to be continue

--- TwoHeaps/sliding_window_median.py
+++ TwoHeaps/sliding_window_median.py
@@ -2,11 +2,19 @@ from heapq import *
 import heapq
 
 class SlidingWindowMedian:
+    def __init__(self):
+        self.maxHeap, self.minHeap = [], []
 
     def find_sliding_window_median(self, nums, k):
-        result = []
-        # TODO: Write your code here
-        return result
+        result = [0.0 for x in range(len(nums) - k + 1)]
+        for i in range(0, len(nums)):
+            if not self.maxHeap or nums[i] <= -self.maxHeap[0]:
+                heappush(self.maxHeap, -nums[i])
+            else:
+                heappush(self.minHeap, nums[i])
+
+            self.rebalance_heaps()
+
 
 def main():
 
