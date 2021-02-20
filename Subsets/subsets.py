fc6470fd21edb647b1db075183e8295ccf2a4014
Taha Uygun <tahauygun@gmail.com>
2021-02-20 09:59:57 -0800

    starting subsets

---
+++ Subsets/subsets.py
@@ -0,0 +1,13 @@
+def find_subsets(nums):
+    subsets = []
+    # TODO: Write your code here
+    return subsets
+
+
+def main():
+
+    print("Here is the list of subsets: " + str(find_subsets([1, 3])))
+    print("Here is the list of subsets: " + str(find_subsets([1, 5, 3])))
+
+
+main()
