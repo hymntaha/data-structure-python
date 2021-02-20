0173c1b30b742c672344be8e7147c105ab7850be
Taha Uygun <tahauygun@gmail.com>
2021-02-20 09:46:12 -0800

    solution for sliding window

--- TwoHeaps/sliding_window_median.py
+++ TwoHeaps/sliding_window_median.py
@@ -15,6 +15,45 @@ class SlidingWindowMedian:
 
             self.rebalance_heaps()
 
+            if i-k+1 >= 0: # if we have at least 'k' elements in the sliding window
+                # add the median to the result array
+                if len(self.maxHeap) == len(self.minHeap):
+                    # we have even number of elements, take the average of middle two elements
+                    result[i - k + 1] = -self.maxHeap[0] / 2.0 +m self.minHeap[0] / 2.0
+                else: # because max-heap will have one more element than the min-heap
+                    result[i - k + 1] = -self.maxHeap[0] / 1.0
+
+                # remove the element going out of the sliding window
+                elementToBeRemoved = nums[i - k + 1]
+                if elementToBeRemoved <= -self.maxHeap[0]:
+                    self.remove(self.maxHeap, -elementToBeRemoved)
+                else:
+                    self.remove(self.minHeap, elementToBeRemoved)
+
+                self.rebalance_heaps()
+
+        return result
+
+    # removes an element from the heap keeping the heap property
+    def remove(self, heap, element):
+        ind = heap.index(element) # find the element
+        # move the element to the end and delete it
+        heap[ind] = heap[-1]
+        del heap[-1]
+        # we can use heapify to readjust the elements but that would be O(N)
+        # instead, we will adjust only one element which will O(logN)
+        if ind < len(heap):
+            heapq._siftup(heap, ind)
+            heapq._siftdown(heap, 0, ind)
+
+    def rebalance_heaps(self):
+        # either both the heaps will have equal number of elements or max-heap will have
+        # once more element than the min-heap
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            heappush(self.minHeap, -heappop(self.maxHeap))
+        elif len(self.maxHeap) < len(self.minHeap):
+            heappush(self.maxHeap, -heappop(self.minHeap))
+
 
 def main():
 
