9b438bfc8eb34280ae8d7b5992c0ad69c08289e4
Taha Uygun <tahauygun@gmail.com>
2021-02-20 10:02:49 -0800

    solution for subset

--- Subsets/subsets.py
+++ Subsets/subsets.py
@@ -1,6 +1,17 @@
 def find_subsets(nums):
     subsets = []
-    # TODO: Write your code here
+
+    # start by adding the empty subset
+    subsets.append([])
+    for currentNumber in nums:
+        # we will take all existing subsets and insert the current number in them to create new subsets
+        n = len(subsets)
+        for i in range(n):
+            # create a new subset from the existing subset and insert the current element to it
+            set1 = list(subsets[i])
+            set1.append(currentNumber)
+            subsets.append(set1)
+
     return subsets
 
 
