a1781587d6cf5189ddba627605bdbcc8f25aca49
Taha Uygun <tahauygun@gmail.com>
2021-02-21 22:05:57 -0800

    recursive solution

--- Subsets/permutation.py
+++ Subsets/permutation.py
@@ -41,4 +41,23 @@ Therefore the overall space complexity to store N! permutations each containing 
 
 
 ##### RECURSIVE SOLUTION:
+def generate_permutations(nums):
+    result = []
+    generate_permutations_recursive(nums, 0, [], result)
+    return result
 
+def generate_permutations_recursive(nums, index, currentPermutation, result):
+    if index == len(nums):
+        result.append(currentPermutation)
+    else:
+        # create a new permutation by adding the current number at every position
+        for i in range(len(currentPermutation)+1):
+            newPermutation = list(currentPermutation)
+            newPermutation.insert(i, nums[index])
+            generate_permutations_recursive(nums, index+1, newPermutation,result)
+
+def main():
+    print("Here are all the permutations: " + str(generate_permutations([1, 3, 5])))
+
+
+main()
