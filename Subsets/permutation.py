58ec4c9ef800ec94ef5367cfb508e9eaa76e1db6
Taha Uygun <tahauygun@gmail.com>
2021-02-21 21:20:54 -0800

    analysis

--- Subsets/permutation.py
+++ Subsets/permutation.py
@@ -27,3 +27,14 @@ def main():
 
 
 main()
+
+"""
+TIME COMPLEXITY: We know that there are a total of N! permutations of a set with 'N' numbers. In the algorithm above,
+we are iterating through all of these permutations with the help of the two "for" loops. In each iterations, we go 
+through all the current permutations to insert a new number in them. (Line 17). To insert a number into a permutation of 
+size 'N' will take O(N), which makes the overall time complexity of out algorithm O(N * N!)
+
+SPACE COMPLEXITY: All the additional space used by our algorithm is for the result list and the queue to store the intermediate
+permutations. If you see closely, at any time, we don't have more than N! permutations between the result list and the queue.
+Therefore the overall space complexity to store N! permutations each containing N elements will be O(N*N!)
+"""
