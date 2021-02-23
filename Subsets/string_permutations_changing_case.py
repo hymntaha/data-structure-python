2caf99fb08e922dd8a5f71f77d50e774c703f857
Taha Uygun <tahauygun@gmail.com>
2021-02-22 22:29:40 -0800

    solution for string perm

---
+++ Subsets/string_permutations_changing_case.py
@@ -0,0 +1,26 @@
+def find_letter_case_string_permutations(str):
+    permutations = []
+
+    permutations.append(str)
+    # process every character of the string one by one
+    for i in range(len(str)):
+        if str[i].isalpha(): # only process characters, skip digits
+            # we will take all existing permutations and change the letter case appropriately
+            n = len(permutations)
+            for j in range(n):
+                chs = list(permutations[j])
+                # if the current character is in upper case, change it to lower case or vice versa
+                chs[i] = chs[i].swapcase()
+                permutations.append(''.join(chs))
+
+    return permutations
+
+
+def main():
+    print("String permutations are: " +
+          str(find_letter_case_string_permutations("ad52")))
+    print("String permutations are: " +
+          str(find_letter_case_string_permutations("ab7c")))
+
+
+main()
--- Subsets/subsets.py
+++ Subsets/subsets.py
@@ -18,7 +18,7 @@ def find_subsets(nums):
 def main():
 
     # print("Here is the list of subsets: " + str(find_subsets([1, 3])))
-    print("Here is the list of subsets: " + str(find_subsets([1, 5, 3])))
+    print("Here is the list of subsets: " + str(find_subsets([1, 3, 5])))
 
 
 main()
