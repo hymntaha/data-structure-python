a39ef1d23e4c2a103ef4c5bff0dd92f34da5a473
Taha Uygun <tahauygun@gmail.com>
2021-02-28 13:31:18 -0800

    solution for subset without duplicates

--- Subsets/subsets_without_duplicates.py
+++ Subsets/subsets_without_duplicates.py
@@ -1,22 +1,22 @@
 def find_subsets(nums):
-    # sort the numbers to handle duplicates
-    list.sort(nums)
-    subsets = []
-    subsets.append([])
+    nums.sort()
+    subset = []
+    subset.append([])
     startIndex, endIndex = 0, 0
     for i in range(len(nums)):
         startIndex = 0
-        # if current and the previous elements are same, create new subsets only from the subsets
-        # added in the previous step
-        if i>0 and nums[i] == nums[i-1]:
-            startIndex = endIndex - 1
-        endIndex = len(subsets) - 1
-        for j in range(startIndex, endIndex + 1):
-            # create a new subset from the existing subset and add the current element to it
-            set1 = list(subsets[j])
+
+        if i > 0 and nums[i] == nums[i-1]:
+            startIndex = endIndex + 1
+
+        endIndex = len(subset) - 1
+        for j in range(startIndex, endIndex+1):
+            set1 = list(subset[j])
             set1.append(nums[i])
-            subsets.append(set1)
-    return subsets
+            subset.append(set1)
+
+    return subset
+
 
 
 
