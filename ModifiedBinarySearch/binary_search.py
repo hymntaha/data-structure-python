063df79537d8e90e2e9b3fbb339f0e3f4c7cc79d
Taha Uygun <tahauygun@gmail.com>
2021-03-02 21:13:03 -0800

    adding analysis

--- ModifiedBinarySearch/binary_search.py
+++ ModifiedBinarySearch/binary_search.py
@@ -23,3 +23,10 @@ def binary_search(arr,key):
 
 
 print(binary_search([4,6,10], 10))
+
+"""
+Time complexity: Since, we are reducing the search range by half at every step, this means that the time complexity of our 
+algorithm will be O(logN) where 'N' is the total elements in the given array
+
+Space complexity: The algorithm runs in constant space O(1)
+"""
