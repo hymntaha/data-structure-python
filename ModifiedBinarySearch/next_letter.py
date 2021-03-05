fa5cd772d8e052b0d9a873a3a2069d02c8af1e4a
Taha Uygun <tahauygun@gmail.com>
2021-03-04 23:27:23 -0800

    solution for next letter

---
+++ ModifiedBinarySearch/next_letter.py
@@ -0,0 +1,19 @@
+def search_next_letter(letters,key):
+    n = len(letters)
+    if key<letters[0] or key > letters[n-1]:
+        return letters[0]
+
+    start,end = 0,n-1
+    while start <= end:
+        mid = start + (end-start) //2
+        if key < letters[mid]:
+            end = mid - 1
+        else: # key >= letters[mid]:
+            start = mid + 1
+
+    return[start % n]
+
+
+print(search_next_letter(['a', 'c', 'f', 'h'], 'f'))
+print(search_next_letter(['a', 'c', 'f', 'h'], 'b'))
+print(search_next_letter(['a', 'c', 'f', 'h'], 'm'))
