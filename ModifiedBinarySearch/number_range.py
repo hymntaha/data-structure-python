ab3b38389bec4f39c1ca25490c60973d6a04db67
Taha Uygun <tahauygun@gmail.com>
2021-03-04 23:39:25 -0800

    start

---
+++ ModifiedBinarySearch/number_range.py
@@ -0,0 +1,12 @@
+def find_range(arr, key):
+    result = [- 1, -1]
+    # TODO: Write your code here
+    return result
+
+def main():
+    print(find_range([4, 6, 6, 6, 9], 6))
+    print(find_range([1, 3, 8, 10, 15], 10))
+    print(find_range([1, 3, 8, 10, 15], 12))
+
+
+main()
