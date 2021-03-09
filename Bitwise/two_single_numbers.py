9dd3a71128ca80243927ab30097044f59d293359
Taha Uygun <tahauygun@gmail.com>
2021-03-08 21:16:12 -0800

    start

---
+++ Bitwise/two_single_numbers.py
@@ -0,0 +1,11 @@
+def find_single_numbers(nums):
+    # TODO: Write your code here
+    return [-1, -1]
+
+
+def main():
+    print('Single numbers are:' +
+          str(find_single_numbers([1, 4, 2, 1, 3, 5, 6, 2, 3, 5])))
+    print('Single numbers are:' + str(find_single_numbers([2, 1, 3, 2])))
+
+main()
