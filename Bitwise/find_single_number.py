2975422ddd0bdae9ef428f6bb1dcf4c96d24e591
Taha Uygun <tahauygun@gmail.com>
2021-03-08 21:15:36 -0800

    analysis

--- Bitwise/find_single_number.py
+++ Bitwise/find_single_number.py
@@ -1,10 +1,20 @@
 def find_single_number(arr):
     num = 0
     for i in arr:
-        num ^= i
+        num = num ^ i
 
     return num
 
 
 arr = [1, 4, 2, 1, 3, 2, 3]
 print(find_single_number(arr))
+
+"""
+001 - 1
+010 - 2
+011 - 3
+100 - 4
+101 - 5
+110 - 6
+111 - 7
+"""
