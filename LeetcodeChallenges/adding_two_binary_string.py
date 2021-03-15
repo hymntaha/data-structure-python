665d8c36b052abb5adad0310cec779952a1df1d0
Taha Uygun <tahauygun@gmail.com>
2021-03-14 21:02:40 -0700

    second solution for add binary

--- LeetcodeChallenges/adding_two_binary_string.py
+++ LeetcodeChallenges/adding_two_binary_string.py
@@ -4,3 +4,21 @@ def addBinary(a,b):
 a = "1010"
 b = "1011"
 print(addBinary(a,b))
+
+def addBinary2(a,b):
+    carry = 0
+    result = ''
+
+    a = list(a)
+    b = list(b)
+
+    while a or b or carry:
+        if a:
+            carry += int(a.pop())
+        if b:
+            carry += int(b.pop())
+
+        result += str(carry % 2)
+        carry //=2
+
+    return result[::-1]
