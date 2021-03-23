9571889b2dbe28130f73d7e5465c731ef9bf0aa7
Taha Uygun <tahauygun@gmail.com>
2021-03-22 22:30:03 -0700

    solution for partition K

--- LeetcodeChallenges/divide_list.py
+++ LeetcodeChallenges/divide_list.py
@@ -1,6 +1,35 @@
-def divide_list(arr, n):
-
-    return False
+def divide_list(nums, k):
+    sigma = sum(nums)
+    target, r = divmod(sigma, k)
+    if r != 0:
+        return False
+    nums.sort(reverse=True)
+    n = k
+    s = 0
+    while s < len(nums):
+        if nums[s] < target:
+            break
+        elif nums[s] > target:
+            return False
+        else:
+            n -= 1
+        s += 1
+    bins = [target] * n
+    # remain = target * n
+    def search(nums, s, bins, remain):
+        if remain == 0:
+            return True
+        for i in range(len(bins)):
+            if nums[s] <= bins[i]:
+                bins[i] -= nums[s]
+                if 0 < bins[i] < nums[-1]:
+                    bins[i] += nums[s]
+                    continue
+                if search(nums, s+1, bins, remain-nums[s]):
+                    return True
+                bins[i] += nums[s]
+        return False
+    return search(nums, s, bins, target * n)
 
 print(divide_list([2,3,5,1,2,3],2))
 
