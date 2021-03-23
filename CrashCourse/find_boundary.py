6966686acee42b36fc9769bcdd20ab068bc62023
Taha Uygun <tahauygun@gmail.com>
2021-03-22 23:14:27 -0700

    solution for the boundary

---
+++ CrashCourse/find_boundary.py
@@ -0,0 +1,13 @@
+def find_boundary(arr):
+    left, right = 0, len(arr) - 1
+    boundary_index = -1
+
+    while left <= right:
+        mid = (left + right) //2
+        if arr[mid]:
+            boundary_index = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return boundary_index
