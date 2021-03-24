3c610ec6d41d06aa51532f752c2e2b0a133bf49c
Taha Uygun <tahauygun@gmail.com>
2021-03-23 22:00:34 -0700

    solution for first occurance

---
+++ CrashCourse/find_first_occurrence.py
@@ -0,0 +1,18 @@
+def find_first_occurrence(arr, target):
+    left, right = 0, len(arr)-1
+    boundary_index = -1
+    while left <= right:
+        mid = (left+right) // 2
+        if arr[mid] == target:
+            boundary_index = mid
+            right = mid - 1
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return boundary_index
+
+arr = [1, 3, 3, 3, 3, 6, 10, 10, 10, 100]
+target = 3
+print(find_first_occurrence(arr,target))
