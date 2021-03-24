904a5d367ef65a5b033a18e0c9e30aab49a0e9e5
Taha Uygun <tahauygun@gmail.com>
2021-03-23 21:48:03 -0700

    solution for first not smaller

--- CrashCourse/first_not_smaller.py
+++ CrashCourse/first_not_smaller.py
@@ -1,3 +1,16 @@
 def first_not_smaller(arr, target):
-    # WRITE YOUR CODE HERE:
-    pass
+    left, right = 0, len(arr)-1
+    boundary_index = -1
+    while left <= right:
+        mid = (left+right) // 2
+        if arr[mid] >= target:
+            boundary_index = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return boundary_index
+
+arr = [1, 3, 3, 5, 8, 8, 10]
+target = 2
+print(first_not_smaller(arr,target))
+
