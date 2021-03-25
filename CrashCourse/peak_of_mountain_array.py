31c98e06f29f83e1327fb9e13bc41caa3d681624
Taha Uygun <tahauygun@gmail.com>
2021-03-24 23:16:18 -0700

    solution

---
+++ CrashCourse/peak_of_mountain_array.py
@@ -0,0 +1,13 @@
+def peak_of_mountain_array(arr):
+    left, right = 0, len(arr) - 1
+    peak = -1
+
+    while left <= right:
+        mid = (left+right) // 2
+        if mid == len(arr) - 1 or arr[mid] >= arr[mid + 1]:
+            peak = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return peak
