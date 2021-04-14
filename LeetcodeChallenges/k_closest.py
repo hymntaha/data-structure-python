104de70093ec6899d599ac94ab97e9e65d24e52e
Taha Uygun <tahauygun@gmail.com>
2021-04-13 22:24:02 -0700

    solution

--- LeetcodeChallenges/k_closest.py
+++ LeetcodeChallenges/k_closest.py
@@ -16,3 +16,11 @@ def kClosest(points, K):
 points = [[3,3],[5,-1],[-2,4]]
 k = 2
 print(kClosest(points,k))
+
+"""
+Inserting an item to a heap of size k take O(logK) time.
+And we do this for each item points.
+So runtime is O(N*logK) where N is the length of points.
+
+Space: O(K) for our heap
+"""
