c972f3196e5d1cbc8ce7ce373043381e50b71758
Taha Uygun <tahauygun@gmail.com>
2021-04-13 22:55:04 -0700

    solution for valid palindrome

---
+++ LeetcodeChallenges/valid_palindrome2.py
@@ -0,0 +1,12 @@
+def validPalindrome(s):
+    n = len(s)
+    l = 0
+    r = n - 1
+    while l <= r:
+        if s[l] == s[r]:
+            l += 1
+            r -= 1
+        else:
+            break
+
+    return s[l:r] == s[l:r][::-1] or s[l+1:r+1] == s[l+1:r+1][::-1]
