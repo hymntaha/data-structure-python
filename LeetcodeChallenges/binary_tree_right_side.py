37bab5afbe86d3b52386a3a6ab10e7c3f601fb15
Taha Uygun <tahauygun@gmail.com>
2021-04-15 22:25:29 -0700

    analysis

--- LeetcodeChallenges/binary_tree_right_side.py
+++ LeetcodeChallenges/binary_tree_right_side.py
@@ -6,3 +6,8 @@ def rightSideView(root):
             view += level[-1].val,
             level = [kid for node in level for kid in (node.left, node.right) if kid]
     return view
+
+"""
+Time complexity: O(N)
+Space complexity: O(N)
+"""
