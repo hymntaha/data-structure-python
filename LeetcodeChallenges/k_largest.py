8f52f8bf5222e0ef33ea73a680cddf17b0ef79aa
Taha Uygun <tahauygun@gmail.com>
2021-04-21 22:01:56 -0700

    solution for kth largest

---
+++ LeetcodeChallenges/k_largest.py
@@ -0,0 +1,18 @@
+import heapq
+def findKthLargest(nums, k):
+    if not nums or not k or k < 0:
+        return None
+
+    maxHeap, res = [], None
+    for num in nums:
+        heapq.heappush(maxHeap, -num)
+
+    for _ in range(k):
+        res = -heapq.heappop(maxHeap)
+    return res
+
+
+nums = [8,5,6,1,3,4,2]
+k = 3
+
+print(findKthLargest(nums,k))
