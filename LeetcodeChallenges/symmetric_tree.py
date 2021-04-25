1c97c1286f14d82f7a5275d6e20c18a439a55d55
Taha Uygun <tahauygun@gmail.com>
2021-04-25 13:39:02 -0700

    iterative

--- LeetcodeChallenges/symmetric_tree.py
+++ LeetcodeChallenges/symmetric_tree.py
@@ -7,3 +7,20 @@ def dfs(self, l, r):
     if l and r:
         return l.val == r.val and self.dfs(l.left, r.right) and self.dfs(l.right, r.left)
     return l == r
+
+
+def isSymmetricIterative(self, root):
+    if not root:
+        return True
+
+    stack = [(root.left, root.right)]
+    while stack:
+        l, r = stack.pop()
+        if not l and not r:
+            continue
+        if not l or not r or (l.val != r.val):
+            return False
+        stack.append((l.left, r.right))
+        stack.append(l.right, r.left)
+
+    return True
