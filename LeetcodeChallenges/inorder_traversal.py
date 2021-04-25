2b883db8b5a8af3c2003cc499b8e1e8b3b2c5fb6
Taha Uygun <tahauygun@gmail.com>
2021-04-24 22:06:47 -0700

    solution for inorder traversal iterative

---
+++ LeetcodeChallenges/inorder_traversal.py
@@ -0,0 +1,12 @@
+def inorderTraversalIterative(root):
+    res, stack = [], []
+    while True:
+        while root:
+            stack.append(root)
+            root = root.left
+        if not stack:
+            return res
+        node = stack.pop()
+        res.append(node.val)
+        root = node.right
+    return res
