c448bc273859a113b4a77eb2d0571dfbf5d203a5
Taha Uygun <tahauygun@gmail.com>
2021-05-06 22:42:00 -0700

    solution for grouping anagram

---
+++ LeetcodeChallenges/group_anagram.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+def groupAnagrams(strs):
+    count = defaultdict(list)
+
+    for word in strs:
+        key = ''.join(sorted(word))
+        count[key].append(word)
+
+    return list(count.values())
+
+input = ["eat","tea","tan","ate","nat","bat"]
+print(groupAnagrams(input))
