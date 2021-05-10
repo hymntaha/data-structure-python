947c9693faab6cb2b04dfeab5595de9b0ed60fa8
taha uygun <tahauygun@gmail.com>
2021-05-09 23:09:32 -0700

    ternary tree solution

---
+++ CrashCourse/ternary_tree.py
@@ -0,0 +1,16 @@
+def ternary_tree_paths(root):
+    # dfs helper function
+    def dfs(root, path, res):
+        # exit condition, reached leaf node, append paths to results
+        if all(c is None for c in root.children):
+            res.append('->'.join(path) + '->' + root.val)
+            return
+
+        # dfs on each non-null child
+        for child in root.children:
+            if child is not None:
+                dfs(child, path + [root.val], res)
+
+    res = []
+    if root:dfs(root, [], res)
+    return res
