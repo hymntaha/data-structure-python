95005a7945cc5274b17910dfff3760197827dd0c
taha uygun <tahauygun@gmail.com>
2021-05-09 23:13:14 -0700

    building tree

--- CrashCourse/ternary_tree.py
+++ CrashCourse/ternary_tree.py
@@ -20,3 +20,14 @@ class Node:
         self.val = val
         self.children = children
 
+def build_tree(nodes):
+    val = next(nodes)
+    if not val or val == 'x': return
+    cur = Node(val, [])
+    for _ in range(3): cur.children.append(build_tree(nodes))
+    return cur
+
+inputs = ["1 2 5 x x x x x 3 x x x 4 x x x", "1 2 3 x x x 4 x x x 7 x x x 5 x x x 6 x x x"]
+for i in range(len(inputs)):
+    root = build_tree(iter(inputs[i].split()))
+    print("Ternary tree path :",ternary_tree_paths(root))
