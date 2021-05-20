cb75a07b5aab140fe83e5c7b42062c547cb9809b
taha uygun <tahauygun@gmail.com>
2021-05-19 21:54:44 -0700

    starting connect all nodes

---
+++ BinaryTree/BFS/connecting_all_nodes.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right, self.next = None, None, None
+
+    # tree traversal using 'next' pointer
+    def print_tree(self):
+        print("Traversal using 'next' pointer: ", end='')
+        current = self
+        while current:
+            print(str(current.val) + " ", end='')
+            current = current.next
+
+
+def connect_all_siblings(root):
+    # TODO: Write your code here
+    return
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    connect_all_siblings(root)
+    root.print_tree()
+
+
+main()
