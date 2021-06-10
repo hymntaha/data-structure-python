9bcfde1e2870b1e752160cfa4bedfc133f6c355a
taha uygun <tahauygun@gmail.com>
2021-06-09 22:21:52 -0700

    lgn sol

--- LeetcodeChallenges/my_pow.py
+++ LeetcodeChallenges/my_pow.py
@@ -1,16 +1,16 @@
 def myPow(x,n):
     if n < 0:
-        x = 1 / x
+        x = 1/x
         n = -n
-
-    pow = 1
-
-    while n:
-        if n & 1:
-            pow *= x
-        x *= x
-        n >>= 1
-    return pow
+    ans = 1
+    while n :
+        if n % 2:
+            ans = x * ans
+            n -= 1
+        else:
+            x = x * x
+            n = n // 2
+    return ans
 
 x = 2.0000
 b = -3
