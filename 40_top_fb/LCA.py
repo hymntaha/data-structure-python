d356c1f78850b2033972a49531d98d3b5ee8a469
taha uygun <tahauygun@gmail.com>
2021-06-27 09:35:22 -0700

    some test cases

---
+++ 40_top_fb/LCA.py
@@ -0,0 +1,37 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        stack = [root]
+        parent = {root: None}
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+        while q not in ancestors:
+            q = parent[q]
+        return q.val
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+solution = Solution()
+
+print(solution.lowestCommonAncestor(root,root.right,root.left.left))
--- 40_top_fb/move_zeroes.py
+++ 40_top_fb/move_zeroes.py
@@ -2,20 +2,21 @@ def move_zeros_to_left(A):
     if len(A) < 1:
         return
 
-    lengthA = len(A)
-    write_index = lengthA - 1
-    read_index = lengthA - 1
+    n = len(A)
+    read_index = n-1
+    write_index = n-1
 
-    while(read_index >= 0):
+    while (read_index >= 0):
         if A[read_index] != 0:
             A[write_index] = A[read_index]
             write_index -= 1
 
         read_index -= 1
 
-    while(write_index >= 0):
-        A[write_index]=0;
-        write_index-=1
+    while (write_index >= 0):
+        A[write_index] = 0
+        write_index -= 1
+    return A
 
 nums=[1,10,20,0,59,63,0,88,0]
 move_zeros_to_left(nums)
