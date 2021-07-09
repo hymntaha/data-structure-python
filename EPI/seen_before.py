1d4b53fd23cb0392fcc01a3581e2df4d2c06b081
taha uygun <tahauygun@gmail.com>
2021-07-08 21:53:23 -0700

    solution for remove duplicates

---
+++ EPI/seen_before.py
@@ -0,0 +1,15 @@
+def seen_before(arr):
+    first = 0
+    second = 1
+
+    while first < len(arr) - 1:
+        if arr[first] == arr[second]:
+            arr.remove(arr[first])
+        else:
+            first += 1
+            second += 1
+
+    return arr
+
+A = [2,3,3,3,3,5,5,6,6,6,7,11,11,11,11,13,16]
+print(seen_before(A))
