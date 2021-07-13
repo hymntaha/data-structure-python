4df9d241bb7b089f9b93e8ea8429e7adb422617a
taha uygun <tahauygun@gmail.com>
2021-07-12 17:04:54 -0700

    other genius solution

--- LeetcodeChallenges/trap_water.py
+++ LeetcodeChallenges/trap_water.py
@@ -27,3 +27,18 @@ def trapWater(bars):
 
 block = [0,1,0,2,1,0,1,3,2,1,2,1]
 print(trapWater(block))
+
+##### other genius solution
+def trapWater2(height):
+    waterLevel = []
+    left = 0
+    for h in height:
+        left = max(left, h)
+        waterLevel += [left] # over-fill it to left max height
+    right = 0
+    for i, h in reversed(list(enumerate(height))):
+        right = max(right, h)
+        waterLevel[i] = min(waterLevel[i], right) - h # drain to the right height
+    return sum(waterLevel)
+
+print(trapWater2(block))
