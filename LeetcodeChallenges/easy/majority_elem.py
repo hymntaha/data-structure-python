ecc4cff2b8a753f06859c34ea2f66149fa6bcac5
taha uygun <tahauygun@gmail.com>
2021-08-02 16:18:59 -0700

    majority element boyer & moore algo

---
+++ LeetcodeChallenges/easy/majority_elem.py
@@ -0,0 +1,14 @@
+def majorityElem(nums):
+    candidate, count = nums[0], 0
+    for num in nums:
+        if num == candidate:
+            count += 1
+        elif count == 0:
+            candidate, count = num, 1
+        else:
+            count -= 1
+    return candidate
+
+
+arr = [2,2,1,1,1,2,2]
+print(majorityElem(arr))
