20e87c122af63b1404f38fd17228414c80dd0f2d
taha uygun <tahauygun@gmail.com>
2021-08-10 22:19:11 -0700

    solution for isAnagram

---
+++ LeetcodeChallenges/easy/valid_anagram.py
@@ -0,0 +1,27 @@
+def isAnagram(s,t):
+    new_string = {}
+
+    for i in s:
+        if i in new_string:
+            new_string[i] += 1
+        else:
+            new_string[i] = 1
+
+    for i in t:
+        if i in new_string:
+            new_string[i] -= 1
+        else:
+            return False
+
+    for i in new_string.items():
+        if i[1] != 0:
+            return False
+
+
+    return True
+
+
+s = 'rat'
+t = 'car'
+
+print(isAnagram(s,t))
