ffcc47f2e0fb334ad0f95daa9deff1ebc3dcb132
taha uygun <tahauygun@gmail.com>
2021-08-20 16:42:56 -0700

    find product solution

---
+++ EducativePythonPath/DS/Lists/find_product.py
@@ -0,0 +1,17 @@
+def find_product(lst):
+    left = 1
+    product = []
+
+    for ele in lst:
+        product.append(left)
+        left = left * ele
+    # get product starting from right
+    right = 1
+    for i in range(len(lst)-1, -1, -1):
+        product[i] = product[i] * right
+        right = right * lst[i]
+
+    return product
+
+
+print(find_product([4,2,3,4,0]))
