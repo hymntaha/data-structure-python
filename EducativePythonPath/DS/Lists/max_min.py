d64841687e2aeeb2b4008ec0399124fda6cd4ea8
taha uygun <tahauygun@gmail.com>
2021-08-22 15:19:09 -0700

    in place solution

--- EducativePythonPath/DS/Lists/max_min.py
+++ EducativePythonPath/DS/Lists/max_min.py
@@ -15,6 +15,28 @@ def max_min(lst):
     return result
 
 
+def min_max2(lst):
+    # Return empty list for empty list
+    if (len(lst) == 0):
+        return []
+
+    maxIdx = len(lst) - 1  # max index
+    minIdx = 0  # first index
+    maxElem = lst[-1] + 1  # Max element
+    # traverse the list
+    for i in range(len(lst)):
+        # even number means max element to append
+        if i % 2 == 0:
+            lst[i] += (lst[maxIdx] % maxElem) * maxElem
+            maxIdx -= 1
+        # odd number means min number
+        else:
+            lst[i] += (lst[minIdx] % maxElem) * maxElem
+            minIdx += 1
+
+    for i in range(len(lst)):
+        lst[i] = lst[i] // maxElem
+    return lst
 
 
 
@@ -22,7 +44,9 @@ def max_min(lst):
 
 
 
-print(max_min([1,2,3,4,5,6,7]))
+
+# print(max_min([1,2,3,4,5,6,7]))
+print(min_max2([1,2,3,4,5,6,7]))
 """
 1,2,3
 4,5,6,7
