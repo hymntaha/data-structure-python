96ca1d72dfd86d3ec720130aab598f4c361ec1d4
taha uygun <tahauygun@gmail.com>
2021-08-26 22:50:48 -0700

    solution for remove duplicates

--- EducativePythonPath/DS/Linked Lists/insertion_tail.py
+++ EducativePythonPath/DS/Linked Lists/insertion_tail.py
@@ -139,6 +139,33 @@ def find_mid(lst):
 
     return middle_node
 
+def remove_duplicates(lst):
+    if lst.is_empty():
+        return None
+
+    # If list only has one node, leave it unchanged
+    if lst.get_head().next_element is None:
+        return lst
+
+    outer_node = lst.get_head()
+    while outer_node:
+        inner_node = outer_node  # Iterator for the inner loop
+        while inner_node:
+            if inner_node.next_element:
+                if outer_node.data == inner_node.next_element.data:
+                    # Duplicate found, so now removing it
+                    new_next_element = inner_node.next_element.next_element
+                    inner_node.next_element = new_next_element
+                else:
+                    # Otherwise simply iterate ahead
+                    inner_node = inner_node.next_element
+            else:
+                # Otherwise simply iterate ahead
+                inner_node = inner_node.next_element
+        outer_node = outer_node.next_element
+
+    return lst
+
 lst = LinkedList()
 lst.insert_at_head(1)
 lst.insert_at_head(4)
