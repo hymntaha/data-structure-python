9f75c7048510953af4de305b7091f30e661001b9
taha uygun <tahauygun@gmail.com>
2021-09-29 16:04:46 -0700

    solution for remove even nums

--- EducativePythonPath/DS/Lists/removeEven.py
+++ EducativePythonPath/DS/Lists/removeEven.py
@@ -1,24 +1,11 @@
 def removeEven(arr):
-    left = 0
-    right = 1
+    new_arr = []
 
-    while right <= len(arr) - 1:
-        if arr[left] % 2 == 1:
-            left += 1
-            right += 1
-        elif arr[left] % 2 == 0 and arr[right] % 2 == 0:
-            right += 1
-        elif arr[right] % 2 == 1:
-            arr[left],arr[right] = arr[right],arr[left]
-            left += 1
-            right += 1
+    for i in arr:
+        if i % 2 != 0:
+            new_arr.append(i)
+    return new_arr
 
-    for i in range(len(arr) - 1, -1, -1):
-        if arr[i] % 2 == 0:
-            arr.pop()
-
-
-    return arr
 
 nums = [1,2,4,5,10,6,3]
 """
