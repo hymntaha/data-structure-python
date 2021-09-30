b5ceabfa3c2932a62597e41efbbb8b22405c1ab4
taha uygun <tahauygun@gmail.com>
2021-09-29 21:53:08 -0700

    solution for merging 2 sorted arrays

--- EducativePythonPath/DS/Lists/merge_two_sorted_list.py
+++ EducativePythonPath/DS/Lists/merge_two_sorted_list.py
@@ -1,34 +1,30 @@
 def mergeTwoSortedList(lst1,lst2):
-    index_arr1 = 0
-    index_arr2 = 0
-    index_result = 0
+    x = 0
+    y = 0
+    m = 0
     result = []
 
-    for i in range(len(lst1)+len(lst2)):
+    for i in range(len(lst1) + len(lst2)):
         result.append(i)
-    # Traverse Both lists and insert smaller value from arr1 or arr2
-    # into result list and then increment that lists index.
-    # If a list is completely traversed, while other one is left then just
-    # copy all the remaining elements into result list
-    while (index_arr1 < len(lst1)) and (index_arr2 < len(lst2)):
-        if (lst1[index_arr1] < lst2[index_arr2]):
-            result[index_result] = lst1[index_arr1]
-            index_result += 1
-            index_arr1 += 1
+
+    while (x < len(lst1)) and (y < len(lst2)):
+        if lst1[x] <= lst2[y]:
+            result[m] = lst1[x]
+            x += 1
+            m += 1
         else:
-            result[index_result] = lst2[index_arr2]
-            index_result += 1
-            index_arr2 += 1
-    while (index_arr1 < len(lst1)):
-        result[index_result] = lst1[index_arr1]
-        index_result += 1
-        index_arr1 += 1
-    while (index_arr2 < len(lst2)):
-        result[index_result] = lst2[index_arr2]
-        index_result += 1
-        index_arr2 += 1
+            result[m] = lst2[y]
+            y += 1
+            m += 1
+
+    while x < len(lst1):
+        result[m] = lst1[x]
+        x += 1
+        m += 1
+    while y < len(lst2):
+        result[m] = lst2[y]
+        y += 1
+        m += 1
+
     return result
-
-
-# print(mergeTwoSortedList([4, 5, 6], [-2, -1, 0, 7]))
 print(mergeTwoSortedList([1, 3, 4, 5],[2, 6, 7, 8]))
