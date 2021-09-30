de9d24b22b45a916ab2c095208d45ec6e8427fbe
taha uygun <tahauygun@gmail.com>
2021-09-29 22:03:21 -0700

    adding sorted solution

--- EducativePythonPath/DS/Lists/find_sum.py
+++ EducativePythonPath/DS/Lists/find_sum.py
@@ -5,6 +5,26 @@ def find_sum(lst,k):
 
     return False
 
+# Linear scan takes O(N) and sorting O(nlogn). Overall O(nlogn).
+def find_sum2(lst,k):
+    lst.sort()
+    index1 = 0
+    index2 = len(lst) - 1
+    result = []
+    sum = 0
+
+    while index1 != index2:
+        sum = lst[index1] + lst[index2]
+        if sum < k:
+            index1 += 1
+        elif sum > k:
+            index2 -= 1
+        else:
+            result.append(lst[index1])
+            result.append(lst[index2])
+            return result
+    return False
+
 lst = [1,21,3,14,5,60,7,6]
 k = 81
-print(find_sum(lst,k))
+print(find_sum2(lst,k))
