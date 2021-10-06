34dace71a447fffb2a2f9a20316aed7320a0d478
taha uygun <tahauygun@gmail.com>
2021-10-05 21:22:00 -0700

    bubble sort

---
+++ EducativePythonPath/Algo/Sorting/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(lst):
+
+    for i in range(len(lst)):
+        #Last element is already in place
+        for j in range(0, len(lst) - i -1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j],
+
+    return lst
+
+print(bubble_sort([3,2,1,5,4]))
