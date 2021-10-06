f8e08c5d3f87e1115acc86da10e533a8db34ec6f
taha uygun <tahauygun@gmail.com>
2021-10-05 21:42:21 -0700

    merge sort

---
+++ EducativePythonPath/Algo/Sorting/merge_sort.py
@@ -0,0 +1,39 @@
+def merge_sort(lst):
+
+    if len(lst) > 1:
+        mid = len(lst) // 2
+        left = lst[:mid]
+        right = lst[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0
+        j = 0
+        k = 0
+
+        # Copy data to temp lists left[] and right[]
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst[k] = left[i]
+                i+=1
+            else:
+                lst[k] = right[j]
+                j+=1
+            k +=1
+
+        # Checking if any element was left
+        while i < len(left):
+            lst[k] = left[i]
+            i += 1
+            k += 1
+
+        # Checking if any element was right
+        while j < len(right):
+            lst[k] = right[j]
+            j += 1
+            k += 1
+    return lst
+
+
+print(merge_sort([3,2,1,5,4]))
