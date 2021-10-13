02cc7cc76e3d834128094805747c0f099726144f
taha uygun <tahauygun@gmail.com>
2021-10-12 21:26:18 -0700

    solution for max prod

--- LeetcodeChallenges/max_prod.py
+++ LeetcodeChallenges/max_prod.py
@@ -1,15 +1,17 @@
 def maxProd(nums):
-    maxProd = nums[0]
-    currProd = 1
+    res = max(nums)
+    curMin, curMax = 1,1
 
-    for i in nums:
-        if currProd < 0:
-            currProd = 1
-        currProd = currProd * i
-        maxProd = max(maxProd, currProd)
+    for n in nums:
+        if n == 0:
+            curMin, curMax = 1,1
+            continue
+        tmp = curMax * n
+        curMax = max(n * curMax, n * curMin, n)
+        curMin = min(tmp, n * curMin, n)
+        res = max(res, curMax, curMin)
 
-    return maxProd
+    return res
 
-
-nums = [-3, -1, -1]
+nums = [2,3,-2,4]
 print(maxProd(nums))
