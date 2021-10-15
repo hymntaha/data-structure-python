02261c32ac1b7224d6d85c833aca5ecaae8a9809
taha uygun <tahauygun@gmail.com>
2021-10-14 21:23:02 -0700

    O(n) solution

--- LeetcodeChallenges/contains_most_water.py
+++ LeetcodeChallenges/contains_most_water.py
@@ -13,7 +13,19 @@ def maxArea(height):
     #         l += 1
     #         r = l + 1
     # return maxWater
+    res = 0
+    l,r = 0, len(height) - 1
 
+    while l < r:
+        area = (r-l) * min(height[l], height[r])
+        res = max(res, area)
+
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+
+    return res
 
 lst = [1,8,6,2,5,4,8,3,7]
 print(maxArea(lst))
