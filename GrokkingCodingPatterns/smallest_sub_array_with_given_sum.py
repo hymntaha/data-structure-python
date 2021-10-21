bf97dc2b6f004db1217effb693a6a2b9c8852f10
taha uygun <tahauygun@gmail.com>
2021-10-20 20:46:53 -0700

    solution for smallest win subarray

---
+++ GrokkingCodingPatterns/smallest_sub_array_with_given_sum.py
@@ -0,0 +1,22 @@
+import math
+
+
+def smallest_subarray_with_given_sum(s, arr):
+    minLen = float('inf')
+    windowStart, winSum = 0, 0
+
+    for windowEnd in range(len(arr)):
+        winSum += arr[windowEnd]
+
+        while winSum >= s:
+            minLen = min(minLen, windowEnd-windowStart+1)
+            winSum -= arr[windowStart]
+            windowStart += 1
+
+    if minLen == math.inf: return 0
+    return minLen
+
+
+inp = [2, 1, 5, 2, 3, 2]
+S = 7
+print(smallest_subarray_with_given_sum(S, inp))
