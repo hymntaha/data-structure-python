8b87646a23fee249c7d757d9d153a794accf868b
taha uygun <tahauygun@gmail.com>
2021-10-20 20:52:39 -0700

    adding space & time complexity

--- GrokkingCodingPatterns/smallest_sub_array_with_given_sum.py
+++ GrokkingCodingPatterns/smallest_sub_array_with_given_sum.py
@@ -16,7 +16,10 @@ def smallest_subarray_with_given_sum(s, arr):
     if minLen == math.inf: return 0
     return minLen
 
-
+'''
+Time complexity: O(N+N) -> O(N)
+Space complexity: O(1)
+'''
 inp = [2, 1, 5, 2, 3, 2]
 S = 7
 print(smallest_subarray_with_given_sum(S, inp))
