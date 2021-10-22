d903bec6e40e48310ae847a6cf680d41419c269a
taha uygun <tahauygun@gmail.com>
2021-10-21 21:58:56 -0700

    repeating solutions

--- GrokkingCodingPatterns/max_sub_array.py
+++ GrokkingCodingPatterns/max_sub_array.py
@@ -1,9 +1,17 @@
 def maxSubArraySizeK(arr,k):
+    window_start, max_sum = 0, 0
+    cur_sum = 0
+    for window_end in range(len(arr)):
+        cur_sum += arr[window_end]
 
+        if window_end > k - 1:
+            cur_sum -= arr[window_start]
+            window_start += 1
 
+        max_sum = max(max_sum, cur_sum)
 
+    return max_sum
 
-
-inp = [2, 1, 5, 1, 3, 2]
-k = 3
+inp = [2, 3, 4, 1, 5]
+k = 2
 print(maxSubArraySizeK(inp,k))
--- GrokkingCodingPatterns/smallest_sub_array_with_given_sum.py
+++ GrokkingCodingPatterns/smallest_sub_array_with_given_sum.py
@@ -1,12 +1,25 @@
 import math
 
 def smallest_subarray_with_given_sum(s, arr):
+    window_start = 0
+    window_sum = 0
+    min_length = math.inf
+
+    for window_end in range(len(arr)):
+        window_sum += arr[window_end]
+
+        while window_sum >= s:
+            min_length = min(min_length, window_end-window_start+1)
+            window_sum -= arr[window_start]
+            window_start += 1
+
+    return min_length
 
 
 '''
 Time complexity: O(N+N) -> O(N)
 Space complexity: O(1)
 '''
-inp = [2, 1, 5, 2, 3, 2]
-S = 7
+inp = [3, 4, 1, 1, 6]
+S = 8
 print(smallest_subarray_with_given_sum(S, inp))
