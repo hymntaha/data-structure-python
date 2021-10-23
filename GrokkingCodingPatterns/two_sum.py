d40222ea2d04328a4004ef3e1046e265e3295ab4
taha uygun <tahauygun@gmail.com>
2021-10-23 15:58:50 -0700

    alternative solution for 2 sum

--- GrokkingCodingPatterns/two_sum.py
+++ GrokkingCodingPatterns/two_sum.py
@@ -6,6 +6,15 @@ def two_sum(arr,tar):
     #     else:
     #         nums[arr[i]] = i
     # return [-1, -1]
+    l,r = 0, len(arr) - 1
+    while l<r:
+        if arr[l] + arr[r] == tar:
+            return [l,r]
+        elif arr[l] + arr[r] > tar:
+            r -= 1
+        else:
+            l += 1
+    return [-1,-1]
 
 array = [1, 2, 3, 4, 6]
 target=6
