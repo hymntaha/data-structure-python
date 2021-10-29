303287b5f4c4e086d62d3458ffe3e07bf7d4f383
taha uygun <tahauygun@gmail.com>
2021-10-28 21:38:33 -0700

    adding analysis

--- GrokkingCodingPatterns/happy_number.py
+++ GrokkingCodingPatterns/happy_number.py
@@ -19,3 +19,26 @@ def find_square_sum(num):
 
 print(find_happy_number(23))
 print(find_happy_number(12))
+''' 
+Time complexity: The time complexity of the algorithm is diffucult 
+to determine. However we know the fact that all unhappy number eventually
+get stuck in the cycle:
+
+4->16->37->58->89->145->42->20->4
+
+This sequence behavior tells us 2 things:
+
+1- If the number N is less than or equal to 1000, then we reach the cycle or 1 in at most 1001 steps.
+2- For N > 1000, suppose the number has 'M' digits and the next number is N1. From the Wikipedia link, we know that 
+the sum of the squares of the digits of N is at most 9^2M, or 81M (this will happen when all digits of N are 9.
+
+This means:
+N1 < 81M
+As we know M = (logN+1)
+Therefore N1 < 81 * log(N+1) => N1 = O(logN)
+
+This concludes that the above algorithm will have a time complexity of O(logN)
+
+Space Complexity:O(1)
+
+'''
