5c8e58711529c2296b9f3b4a04ec38056da827d6
taha uygun <tahauygun@gmail.com>
2021-11-09 23:01:00 -0800

    solution for subsets with distinct values

--- GrokkingCodingPatterns/find_subsets.py
+++ GrokkingCodingPatterns/find_subsets.py
@@ -12,8 +12,21 @@ def find_subsets(nums):
 
 def main():
 
-    print("Here is the list of subsets: " + str(find_subsets([1, 3])))
+    # print("Here is the list of subsets: " + str(find_subsets([1, 3])))
     print("Here is the list of subsets: " + str(find_subsets([1, 5, 3])))
 
 
 main()
+
+'''
+Since in each step, the number of subsets doubles as we add each element to all
+the existing subsets, therefore, we will have a total of O(2^n) subsets, where N is the total
+number of elements in the input set. 
+
+And since we construct a new subset from an existing set, therefore, the time complexity of above algorithm
+will be O(N*2^n)
+
+Space Complexity: All the additional space used by the algo is for the output list. Since we will have a 
+total of O(2^N) subsets, and each subset can take up to O(N) space, therefore, the space complexity 
+of our algorithm will be O(N*2^N)
+'''
---
+++ GrokkingCodingPatterns/subsets_with_duplicates.py
@@ -0,0 +1,27 @@
+def find_subsets(nums):
+    # sort the numbers to handle duplicates
+    list.sort(nums)
+    subsets = []
+    subsets.append([])
+    startIndex, endIndex = 0, 0
+    for i in range(len(nums)):
+        startIndex = 0
+        # if current and the previous elements are same, create new subsets only from the subsets
+        # added in the previous step
+        if i > 0 and nums[i] == nums[i - 1]:
+            startIndex = endIndex + 1
+        endIndex = len(subsets) - 1
+        for j in range(startIndex, endIndex+1):
+            # create a new subset from the existing subset and add the current element to it
+            set1 = list(subsets[j])
+            set1.append(nums[i])
+            subsets.append(set1)
+    return subsets
+
+def main():
+
+    # print("Here is the list of subsets: " + str(find_subsets([1, 3, 3])))
+    print("Here is the list of subsets: " + str(find_subsets([1, 5, 3, 3])))
+
+
+main()
