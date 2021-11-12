2bc7a0042e99e6b3d377a2f50c6c6f12857f6eec
taha uygun <tahauygun@gmail.com>
2021-11-11 20:11:41 -0800

    adding time & space complexity

--- GrokkingCodingPatterns/binary_search.py
+++ GrokkingCodingPatterns/binary_search.py
@@ -20,6 +20,11 @@ def binary_search(arr, key):
                 low = mid + 1
     return -1
 
+
+"""
+Time complexity: O(logN)
+Space complexity: O(1)
+"""
 def main():
     # print(binary_search([4, 6, 10], 10))
     print(binary_search([1, 2, 3, 4, 5, 6, 7], 5))
