6425c7c49a4c0afabff6107fd15a04db355f7e34
taha uygun <tahauygun@gmail.com>
2021-11-11 21:20:18 -0800

    solution for next letter

---
+++ GrokkingCodingPatterns/next_letter.py
@@ -0,0 +1,25 @@
+def search_next_letter(letters, key):
+
+    n = len(letters)
+    if key > letters[n-1]:
+        return letters[0]
+
+    l,h = 0, n - 1
+    while l <= h:
+        mid = l+(h-l) // 2
+        if key < letters[mid]:
+            h = mid - 1
+        else:
+            l = mid + 1
+
+    return letters[l % n]
+
+
+
+def main():
+    print(search_next_letter(['a', 'c', 'f', 'h'], 'f'))
+    print(search_next_letter(['a', 'c', 'f', 'h'], 'b'))
+    print(search_next_letter(['a', 'c', 'f', 'h'], 'm'))
+
+
+main()
