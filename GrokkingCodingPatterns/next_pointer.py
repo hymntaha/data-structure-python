e4f73bb350eaa6a846a3578ea17ec5c6305a1571
taha uygun <tahauygun@gmail.com>
2021-11-27 18:34:42 -0800

    solution for next pointer in BT

---
+++ GrokkingCodingPatterns/next_pointer.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+
+        cur = root
+        next = root.left
+
+        while cur.left:
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
+                cur = cur.next
+            else:
+                cur = next
+                next = cur.next
