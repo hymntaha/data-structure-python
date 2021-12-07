26e7274c7de5f6f08b648e6e727396d3cba25201
taha uygun <tahauygun@gmail.com>
2021-12-07 09:26:27 -0800

    iterative solution

--- AlgoExpert/ClosestValueInBST.py
+++ AlgoExpert/ClosestValueInBST.py
@@ -4,7 +4,16 @@ class BST:
         self.left = None
         self.right = None
 
+def findClosestValueInBst(tree,target):
+    return findClosestValRecursive(tree,target, float('inf'))
+
 '''
+Average:
+Time: O(log(n))
+Space: O(log(n))
+Worse:
+Time: O(log(n))
+Space: O(log(n))
 '''
 def findClosestValRecursive(tree, target, closest):
     if tree is None:
@@ -19,4 +28,23 @@ def findClosestValRecursive(tree, target, closest):
     else:
         return closest
 
-
+'''
+Average:
+Time: O(log(n))
+Space: O(1)
+Worse:
+Time: O(log(n))
+Space: O(1)
+'''
+def findClosestValIter(tree, target, closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target-closest) > abs(target-currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target > currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
