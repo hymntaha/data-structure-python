bc67352d6a136818010e1e887858997f505c158d
taha uygun <tahauygun@gmail.com>
2021-12-09 10:16:49 -0800

    solution for memoized version of fib

--- AlgoExpert/Fib.py
+++ AlgoExpert/Fib.py
@@ -10,5 +10,13 @@ def getNthFib(n):
 
 '''
 Time complexity: O(n)
-Space complexity: O(n)
+Space complexity: O(1)
 '''
+
+##### Memoized version #######
+def getNthFib(n, memo={1:0, 2:1}):
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n] = getNthFib(n-1, memo) + getNthFib(n-2, memo)
+    return memo[n]
