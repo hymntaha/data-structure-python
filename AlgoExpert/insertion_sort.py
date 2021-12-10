93305437a8a53bae12bee5801509b87e98a62d82
taha uygun <tahauygun@gmail.com>
2021-12-10 09:23:16 -0800

    solution for insertion sort

---
+++ AlgoExpert/insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(array):
+    indexing_length = range(1,len(array))
+
+    for i in indexing_length:
+        value_to_sort = array[i]
+
+        while array[i-1] > value_to_sort and i>0:
+            array[i], array[i-1] = array[i-1], array[i],
+            i = i - 1
+
+    return array
+
+
+print(insertion_sort([3,5,1,6,9,2,11,7,8]))
