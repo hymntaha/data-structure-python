84a13ff54ef4e5434f101dec9082717d4503dd2e
taha uygun <tahauygun@gmail.com>
2021-12-13 20:00:36 -0800

    complexity analysis

--- AlgoExpert/subarray_prod_sum.py
+++ AlgoExpert/subarray_prod_sum.py
@@ -7,6 +7,9 @@ def prodSum(array, multiplier = 1):
             sum += element
     return sum * multiplier
 
-
+'''
+Time: O(N) Every single element and sub array
+Space: O(D) Depth of the recursion
+'''
 array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
 print(prodSum(array))
