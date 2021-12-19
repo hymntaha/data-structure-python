d08f7219a0f0cd8fef0b5c5fd20d81d53c6147d1
taha uygun <tahauygun@gmail.com>
2021-12-18 18:20:11 -0800

    solution for first and last element in array

---
+++ AlgoExpert/first_last_position.py
@@ -0,0 +1,26 @@
+def searchRange(nums, target):
+    left = binarySearch(nums, target, True)
+    right = binarySearch(nums, target, False)
+    return [left, right]
+
+def binarySearch(nums, target, leftBias):
+    l, r = 0, len(nums) - 1
+    i = -1
+    while l <= r:
+        m = (l+r) // 2
+        if target > nums[m]:
+            l = m + 1
+        elif target < nums[m]:
+            r = m - 1
+        else:
+            i = m
+            if leftBias:
+                r = m - 1
+            else:
+                l = m + 1
+
+    return i
+
+
+
+print(searchRange([5,7,8,8,8,8],8))
