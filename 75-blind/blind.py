0d0dd0f863244201c8172e79454b93a0de7a3d46
hymntaha <tahauygun@gmail.com>
2023-08-23 15:06:30 +0000

    search matrix solution

--- 75-blind/blind.py
+++ 75-blind/blind.py
@@ -33,4 +33,36 @@ def search(nums, target):
 
 # print(search([-1,0,3,5,9,12], 9))
 
+def searchMatrix(matrix, target):
+    ROWS, COLS = len(matrix), len(matrix[0])
+    top, bottom = 0, ROWS - 1
 
+    while top <= bottom:
+        mid = (top+bottom) // 2
+        if target < matrix[mid][0]:
+            bottom = mid - 1
+        elif target > matrix[mid][-1]:
+            top = mid + 1
+        else:
+            break
+
+    if not top <= bottom:
+        return False
+    
+    row = (top+bottom) // 2
+    l, r = 0, COLS - 1
+
+    while l <= r:
+        mid = (l+r) // 2
+
+        if matrix[row][mid] == target:
+            return True
+        if matrix[row][mid] > target:
+            r = mid - 1
+        else:
+            l = mid + 1
+
+    return False
+
+
+print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
